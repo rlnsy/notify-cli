2eb86ef5023abfb78113fa0551e8227cf5b98e5c
rlnsy <rowlindsay@gmail.com>
2020-06-27 15:03:06 -0700

    add unix-style interface

---
+++ _pkg_info_.h
@@ -0,0 +1,3 @@
+#ifndef _INFO_H_
+#define _INFO_VERSION_ "v0.1.0"
+#endif
--- _run_notify_.c
+++ _run_notify_.c
@@ -4,22 +4,36 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "_net_curl_.h"
 #include "_secrets_.h"
+#include "_pkg_info_.h"
 
-int main ()
+int main (int argc, char**argv)
 {
-    char* _MSG_ = "Hello there!";
     _post_struct_ p;
-    memset(&p, 0, sizeof(_post_struct_));
-    p._req_url_ = _SECRETS_NIURL_;
-    sprintf(
-        p._reg_params_, "user=%s&token=%s&message=%s",
-        _SECRETS_USERN_,
-        _SECRETS_TOKEN_,
-        _MSG_);
-    _post_(p);
-    if (p._error_) return 1;
+    for (int opt; (opt = getopt(argc, argv, "vm:h")) != EOF;)
+        switch (opt) {
+            case 'v': 
+                return fprintf(stdout, "Notify %s\n", _INFO_VERSION_);
+                break;
+            case 'm':
+                memset(&p, 0, sizeof(_post_struct_));
+                p._req_url_ = _SECRETS_NIURL_;
+                sprintf(
+                    p._reg_params_, "user=%s&token=%s&message=%s",
+                    _SECRETS_USERN_,
+                    _SECRETS_TOKEN_,
+                    optarg);
+                _post_(p);
+                if (p._error_) return 1;
+                break;
+            case 'h':
+                fprintf(stderr, "Usage: notify [-v|-m <message>]\n");
+                break;
+            case '?':
+                return 1;
+        }
     return 0;
 }
